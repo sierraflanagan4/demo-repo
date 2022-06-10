64ffc522e99f2bbe4552726ddf2f24ba64fba796
Sierra Flanagan <91637973+sierraflanagan4@users.noreply.github.com>
2022-06-10 14:09:23 +0100

    Add files via upload

---
+++ test_script.py
@@ -0,0 +1,12 @@
+# This is a test script to use when learning github 
+
+def hello_world():
+    message = 'hello world'
+    print(message)
+
+
+hello_world()
+
+# I am now editing this file to test git status
+# Test skipping the staging area
+# testing staging
